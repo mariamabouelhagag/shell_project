63e6d7079ff518dfa78dd1898f6e854db27e9e43
mariam <mariam888@gmail.com>
2023-08-28 10:45:49 +0300

    readme, shell.h, shell.c

---
+++ shell.c
@@ -0,0 +1,101 @@
+#include "shell.h"
+/**
+ *  * main - the main function.
+ *   * @tokens: the array of pointers to be execluded.
+ *    *
+ *     * Return: 0.
+ *      */
+
+int main(void)
+{
+		pid_t pid;
+			char **tokens = NULL;
+
+				while (1)
+						{
+									write(STDIN_FILENO, "$ ", 2);
+											tokens = token_save();
+
+													if (tokens[0] != NULL)
+																{
+																				execute_builtin(tokens);
+																						}
+															pid = fork();
+
+																	if (pid == -1)
+																				{
+																								perror("fork");
+																											free_tokens(tokens);
+																														return (-1);
+																																}
+																			if (pid == 0)
+																						{
+																										if (execvp(tokens[0], tokens) == -1)
+																														{
+																																			perror(tokens[0]);
+																																							free_tokens(tokens);
+																																											exit(EXIT_FAILURE);
+																																														}
+																												}
+																					else if (pid > 0)
+																								{
+																												wait(NULL);
+																															free_tokens(tokens);
+																																	}
+																							else
+																										{
+																														free_tokens(tokens);
+																																}
+																								}
+					return (0);
+}#include "shell.h"
+/**
+ *  * main - the main function.
+ *   * @tokens: the array of pointers to be execluded.
+ *    *
+ *     * Return: 0.
+ *      */
+
+int main(void)
+{
+		pid_t pid;
+			char **tokens = NULL;
+
+				while (1)
+						{
+									write(STDIN_FILENO, "$ ", 2);
+											tokens = token_save();
+
+													if (tokens[0] != NULL)
+																{
+																				execute_builtin(tokens);
+																						}
+															pid = fork();
+
+																	if (pid == -1)
+																				{
+																								perror("fork");
+																											free_tokens(tokens);
+																														return (-1);
+																																}
+																			if (pid == 0)
+																						{
+																										if (execvp(tokens[0], tokens) == -1)
+																														{
+																																			perror(tokens[0]);
+																																							free_tokens(tokens);
+																																											exit(EXIT_FAILURE);
+																																														}
+																												}
+																					else if (pid > 0)
+																								{
+																												wait(NULL);
+																															free_tokens(tokens);
+																																	}
+																							else
+																										{
+																														free_tokens(tokens);
+																																}
+																								}
+					return (0);
+}
---
+++ shell.h
@@ -0,0 +1,29 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char **token_save();
+void free_tokens(char **tokens);
+void execute_builtin(char **tokens);
+
+#endif#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char **token_save();
+void free_tokens(char **tokens);
+void execute_builtin(char **tokens);
+
+#endif
